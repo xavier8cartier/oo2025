e1d9dd7bfb0590fc1bed8c498008ec0f1ee45985
xavier8cartier <kpetrov1@tlu.ee>
2025-02-07 16:20:55 +0200

    Esimesed muudatused e-poes

---
+++ 01_07.02_andmemudelid/veebipood/src/main/java/ee/ksaveri/veebipood/entity/Product.java
@@ -0,0 +1,30 @@
+package ee.ksaveri.veebipood.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+// Hibernate
+// automaatselt tekib andmebaasi tabel mis on klassi nimega
+
+//File -> Settins -> Plugins -> JPA Buddy -> Install
+@Getter
+@Setter //encapsulation
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //int
+    private String name;
+    private double price;
+    private String image; // .jpg
+    private boolean active;
+
+}
