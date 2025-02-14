171fefa4e930b6fcf20d3cfb756033bb2a9a9cf0
xavier8cartier <kpetrov1@tlu.ee>
2025-02-14 12:34:05 +0200

    Esimesed sammud JAVAS
    App, Repository, Contoller packages loodud

---
+++ 01_07.02_andmemudelid/veebipood/src/main/java/ee/ksaveri/veebipood/VeebipoodApplication.java
@@ -0,0 +1,13 @@
+package ee.ksaveri.veebipood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeebipoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeebipoodApplication.class, args);
+	}
+
+}
---
+++ 01_07.02_andmemudelid/veebipood/src/test/java/ee/ksaveri/veebipood/VeebipoodApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.ksaveri.veebipood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeebipoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
